c4a0a5e2f14f36ed0db815b4e792365c3fbbc77f
hakevin <37036561+hakevin@users.noreply.github.com>
2019-01-29 10:03:35 -0600

    Add files via upload

---
+++ ConfigClientAboutCompany/src/main/java/com/example/demo/ConfigClientAboutCompanyApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientAboutCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientAboutCompanyApplication.class, args);
+	}
+
+}
+
